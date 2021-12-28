472c9e219487ef8f29c04d717bdc614ce90f61cc
Suraj Kalsi <suraj.kalsi@sky.uk>
2021-12-28 14:21:20 +0000

    Data driven post

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,6 +1,17 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import { useParams } from "react-router";
 import axios from "axios";
 import { useEffect, useState } from "react";
+
+const takeawayPoint = (takeaway) => (
+  <li className="post__item">
+    <h2>
+      <b>{takeaway?.title}</b>
+    </h2>
+
+    <p>{takeaway?.takeaway}</p>
+  </li>
+);
 
 export const Post = () => {
   const { slug } = useParams();
@@ -23,74 +34,13 @@ export const Post = () => {
 
   return (
     <div className="post">
-      <h2 className="post__title">Docker</h2>
+      <h2 className="post__title">{data.title}</h2>
       <hr />
       <div className="post__content">
-        <img
-          alt="post-image"
-          className="post__image"
-          src="https://i.ibb.co/mtMZfHK/docker-containers.jpg"
-        />
+        <img alt="post-image" className="post__image" src={data.image} />
         <br />
         <h3>🔑 KEY TAKEAWAYS</h3>
-        <ul>
-          <li className="post__item">
-            <h2>
-              <b>What is Docker?</b>
-            </h2>
-
-            <p>
-              In publishing and graphic design, Lorem ipsum is a placeholder
-              text commonly used to demonstrate the visual form of a document or
-              a typeface without relying on meaningful content. Lorem ipsum may
-              be used as a placeholder before the final copy is
-            </p>
-          </li>
-          <li className="post__item">
-            <h2>
-              <b>What is a Docker image?</b>
-            </h2>
-            <p>
-              In publishing and graphic design, Lorem ipsum is a placeholder
-              text commonly used to demonstrate the visual form of a document or
-              a typeface without relying on meaningful content. Lorem ipsum may
-              be used as a placeholder before the final copy is
-            </p>
-          </li>
-          <li className="post__item">
-            <h2>
-              <b>What is a Docker container?</b>
-            </h2>
-            <p>
-              In publishing and graphic design, Lorem ipsum is a placeholder
-              text commonly used to demonstrate the visual form of a document or
-              a typeface without relying on meaningful content. Lorem ipsum may
-              be used as a placeholder before the final copy is
-            </p>
-          </li>
-          <li className="post__item">
-            <h2>
-              <b>Docker commands</b>
-            </h2>
-            <p>
-              In publishing and graphic design, Lorem ipsum is a placeholder
-              text commonly used to demonstrate the visual form of a document or
-              a typeface without relying on meaningful content. Lorem ipsum may
-              be used as a placeholder before the final copy is
-            </p>
-          </li>
-          <li className="post__item">
-            <h2>
-              <b>How to create your own Docker Image?</b>
-            </h2>
-            <p>
-              In publishing and graphic design, Lorem ipsum is a placeholder
-              text commonly used to demonstrate the visual form of a document or
-              a typeface without relying on meaningful content. Lorem ipsum may
-              be used as a placeholder before the final copy is
-            </p>
-          </li>
-        </ul>
+        <ul>{data?.takeaways?.map((takeaway) => takeawayPoint(takeaway))}</ul>
       </div>
     </div>
   );
