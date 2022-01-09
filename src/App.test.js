32988af77348bb737cfd499755a669305b796117
Suraj Kalsi <suraj.kalsi@sky.uk>
2022-01-09 18:57:32 +0000

    bad commit 2

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,7 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
 test("renders learn react link", () => {
-  expect(true).toEqual(true);
+  const { container } = render(<App />);
+  expect(container).toMatchSnapshot();
 });
